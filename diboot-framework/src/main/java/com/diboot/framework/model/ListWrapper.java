ffb80c412d21ff5ce76b142287e8b492958a7e92
mazc <mzc2003@qq.com>
2018-11-09 17:49:18 +0800

    添加ListWrapper用于解决Spring无法绑定并校验List<Model>的情况

---
+++ diboot-framework/src/main/java/com/diboot/framework/model/ListWrapper.java
@@ -0,0 +1,30 @@
+package com.diboot.framework.model;
+
+import javax.validation.Valid;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Model的List包装类，用于接收List并绑定校验的情况
+ * @author Mazc@dibo.ltd
+ * @version 2018/11/8
+ * Copyright © www.dibo.ltd
+ */
+public class ListWrapper<T extends BaseModel> {
+
+    @Valid
+    private final List<T> modelList;
+
+    public ListWrapper(final T... models){
+        this.modelList = Arrays.asList(models);
+    }
+
+    public ListWrapper(final List<T> modelList){
+        this.modelList = modelList;
+    }
+
+    public List<T> getModelList(){
+        return modelList;
+    }
+
+}
